efb1eb98e0ed43ff88854069b060388bd797f610
SteveTownsend <SteveTownsend0@gmail.com>
2021-04-03 19:20:14 -0400

    Refactor to separate workflow into its component parts. Added BSA and loose file inventory prior to mesh generation

---
+++ AllGUDMeshGen/MeshHandler.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Bsa;
+using Mutagen.Bethesda.Skyrim;
+using SSEForms = Mutagen.Bethesda.FormKeys.SkyrimSE;
+using nifly;
+
+namespace AllGUD
+{
+    internal class MeshHandler
+    {
+        private static string? meshGenLocation;
+        private static IDictionary<string, ModelType> targetMeshes = new Dictionary<string, ModelType>();
+        private static ISet<FormKey> alternateTextureForms = new HashSet<FormKey>();
+
+        // Check STAT records - rare case, maintain a list of plugins where it matters
+        private static ISet<string> staticMods = new HashSet<string>
+        {
+            "Unique Uniques.esp",
+            "UniqueWeaponsRedone.esp"
+        };
+
+        enum ModelType {
+            Unknown = 0,
+            Sword,
+            Dagger,
+            Mace,
+            Axe,
+            Staff,
+            TwoHandMelee,
+            TwoHandRange,
+            Shield
+        };
+
+        private static bool AddMesh(string modelPath, ModelType modelType)
+        {
+            // Do not add the same model more than once - model reuse is common
+            if (targetMeshes.ContainsKey(modelPath))
+            {
+                return false;
+            }
+            targetMeshes.Add(modelPath, modelType);
+            return true;
+        }
+
+        private static void RecordModel(FormKey record, ModelType modelType, IModelGetter model)
+        {
+            // normalize the model path. Model paths always use the backslash character as separator
+            string modelPath = model.File.ToLower();
+            if (AddMesh(modelPath, modelType))
+            {
+                Console.WriteLine("Model {0} with type {1} added as normalized form {2}", model.File, modelType.ToString(), modelPath);
+            }
+
+            // record weapons with alternate textures for patching later in the workflow
+            if (model.AlternateTextures != null && model.AlternateTextures.Count > 0)
+            {
+                Console.WriteLine("Form {0} has alternate textures", record.ToString());
+                alternateTextureForms.Add(record);
+            }
+        }
+
+        private static void CollateWeapons()
+        {
+            foreach (var weap in ScriptLess.PatcherState.LoadOrder.PriorityOrder.WinningOverrides<IWeaponGetter>())
+            {
+                // skip if no model
+                if (weap == null || weap.Model == null)
+                    continue;
+                // skip non-playable
+                if (weap.MajorFlags.HasFlag(Weapon.MajorFlag.NonPlayable))
+                    continue;
+                // skip scan if no Keywords
+                ModelType modelType = ModelType.Unknown;
+                if (weap.Keywords != null)
+                {
+                    foreach (var keyword in weap.Keywords)
+                    {
+                        if (keyword.FormKey == SSEForms.Skyrim.Keyword.WeapTypeDagger.FormKey)
+                        {
+                            modelType = ModelType.Dagger;
+                            break;
+                        }
+                        if (keyword.FormKey == SSEForms.Skyrim.Keyword.WeapTypeSword.FormKey)
+                        {
+                            modelType = ModelType.Dagger;
+                            break;
+                        }
+                        if (keyword.FormKey == SSEForms.Skyrim.Keyword.WeapTypeWarAxe.FormKey)
+                        {
+                            modelType = ModelType.Axe;
+                            break;
+                        }
+                        if (keyword.FormKey == SSEForms.Skyrim.Keyword.WeapTypeMace.FormKey)
+                        {
+                            modelType = ModelType.Mace;
+                            break;
+                        }
+                        if (keyword.FormKey == SSEForms.Skyrim.Keyword.WeapTypeBattleaxe.FormKey ||
+                            keyword.FormKey == SSEForms.Skyrim.Keyword.WeapTypeGreatsword.FormKey ||
+                            keyword.FormKey == SSEForms.Skyrim.Keyword.WeapTypeWarhammer.FormKey)
+                        {
+                            modelType = ModelType.TwoHandMelee;
+                            break;
+                        }
+                        if (keyword.FormKey == SSEForms.Skyrim.Keyword.WeapTypeBow.FormKey)
+                        {
+                            modelType = ModelType.TwoHandRange;
+                            break;
+                        }
+                        if (keyword.FormKey == SSEForms.Skyrim.Keyword.WeapTypeStaff.FormKey)
+                        {
+                            modelType = ModelType.Staff;
+                            break;
+                        }
+                    }
+                }
+
+                // check animation if weapon type not determined yet
+                if (modelType == ModelType.Unknown && weap.Data != null)
+                {
+                    //currently required for: SSM Spears
+                    if (weap.Data.AnimationType == WeaponAnimationType.OneHandSword)
+                    {
+                        modelType = ModelType.Sword;
+                    }
+                }
+
+                // skip records with indeterminate weapon type
+                if (modelType == ModelType.Unknown)
+                    continue;
+
+                RecordModel(weap.FormKey, modelType, weap.Model);
+            }
+        }
+
+        private static void CollateShields()
+        {
+            foreach (var armor in ScriptLess.PatcherState.LoadOrder.PriorityOrder.WinningOverrides<IArmorGetter>())
+            {
+                // skip if no record
+                if (armor == null)
+                    continue;
+                // skip non-playable in main record or template
+                if (armor.MajorFlags.HasFlag(Armor.MajorFlag.NonPlayable) ||
+                    (armor.BodyTemplate != null && armor.BodyTemplate.Flags.HasFlag(BodyTemplate.Flag.NonPlayable)))
+                    continue;
+
+                // skip if not a Shield
+                if (armor.EquipmentType != null &&
+                    armor.EquipmentType.FormKey == SSEForms.Skyrim.EquipType.Shield.FormKey)
+                {
+                    // Armor Armature holds the model information - use the first and warn if more than one
+                    if (armor.Armature != null && armor.Armature.Count >= 1)
+                    {
+                        if (armor.Armature.Count > 1)
+                        {
+                            Console.WriteLine("Armor {0} has {1) Armature models: using the first", armor.FormKey.ToString(), armor.Armature.Count);
+                        }
+                        var arma = armor.Armature[0];
+                        if (arma != null)
+                        {
+                            
+                            IArmorAddonGetter? armorAddon = arma.TryResolve(ScriptLess.PatcherState.LinkCache);
+                            if (armorAddon != null && armorAddon.WorldModel != null)
+                            {
+                                // process male and female cases
+                                if (armorAddon.WorldModel.Male != null)
+                                {
+                                    RecordModel(armorAddon.FormKey, ModelType.Shield, armorAddon.WorldModel.Male);
+                                }
+                                if (armorAddon.WorldModel.Female != null)
+                                {
+                                    RecordModel(armorAddon.FormKey, ModelType.Shield, armorAddon.WorldModel.Female);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        private static void CollateStatics()
+        {
+            foreach (var target in ScriptLess.PatcherState.LoadOrder.PriorityOrder.WinningOverrides<IStaticGetter>().
+                Where(stat => staticMods.Contains(stat.FormKey.ModKey.FileName)))
+            {
+                // skip if no record or Model
+                if (target == null || target.Model == null)
+                    continue;
+                string modelPath = target.Model.File.ToLower();
+                if (modelPath.Contains("weapon") || modelPath.Contains("armor"))
+                {
+                    if (AddMesh(modelPath, ModelType.Unknown))
+                    {
+                        Console.WriteLine("Model {0} for STAT {1} added as normalized form {2}", target.Model.File, target.FormKey.ToString(), modelPath);
+                    }
+                }
+            }
+        }
+
+        private static void TransformMeshes()
+        {
+            // no op if empty
+            if (targetMeshes.Count == 0)
+            {
+                Console.WriteLine("No meshes require transformation");
+                return;
+            }
+            IDictionary<string, string> bsaFiles = new Dictionary<string, string>();
+            int totalMeshes = targetMeshes.Count;
+
+            ISet<string> looseDone = new HashSet<string>();
+            foreach (var kv in targetMeshes)
+            {
+                // loose file wins over BSA contents
+                string originalFile = ScriptLess.Configuration.meshGenInputFolder + kv.Key;
+                if (File.Exists(originalFile))
+                {
+                    using (NifFile nif = new NifFile())
+                    {
+                        Console.WriteLine("Process mesh from loose file {0}", originalFile);
+                        nif.Load(originalFile);
+                        looseDone.Add(kv.Key);
+                    }
+                }
+                else
+                {
+                    // check for this file in archives
+                    bsaFiles.Add("meshes\\" + kv.Key, kv.Key);
+                }
+            }
+
+            IDictionary<string, string> bsaDone = new Dictionary<string, string>();
+            if (bsaFiles.Count > 0)
+            {
+                // Introspect BSAs to locate meshes not found as loose files. Dups are ignored - first find wins.
+                // ModKey parameter appears immaterial.
+                foreach (var bsaFile in Archive.GetApplicableArchivePaths(GameRelease.SkyrimSE, ScriptLess.PatcherState.DataFolderPath, new ModKey()))
+                {
+                    var bsaReader = Archive.CreateReader(GameRelease.SkyrimSE, bsaFile);
+                    foreach (var bsaMesh in bsaReader.Files.Where(candidate => bsaFiles.ContainsKey(candidate.Path)))
+                    {
+                        string rawPath = bsaFiles[bsaMesh.Path];
+                        if (bsaDone.ContainsKey(rawPath))
+                        {
+                            Console.WriteLine("Mesh {0} from BSA {1} already processed from BSA {2}", bsaMesh.Path, bsaFile, bsaDone[rawPath]);
+                            continue;
+                        }
+
+                        bsaDone.Add(rawPath, bsaFile);
+                        using (Stream meshStream = new MemoryStream((int)bsaMesh.Size))
+                        {
+                            bsaMesh.CopyDataTo(meshStream);
+                            using (NifFile nif = new NifFile())
+                            {
+                                // TODO load NIF from MemoryStream
+                                // TODO transform mesh
+                                Console.WriteLine("Process mesh {0} from BSA {1}", bsaMesh.Path, bsaFile);
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Total meshes {0} - {1} Loose, {2} in BSA", targetMeshes.Count, looseDone.Count, bsaDone.Count);
+            foreach (var mesh in targetMeshes.Where(kv => !looseDone.Contains(kv.Key) && !bsaDone.ContainsKey(kv.Key)))
+            {
+                Console.WriteLine("Mesh {0} not found loose or in BSA", mesh.Key);
+            }
+        }
+
+        // Mesh Generation logic from AllGUD Weapon Mesh Generator.pas
+        public static void Generate()
+        {
+            // determine the file path for meshes
+            meshGenLocation = String.IsNullOrEmpty(ScriptLess.Configuration!.meshGenInputFolder) ?
+                ScriptLess.PatcherState!.DataFolderPath : ScriptLess.Configuration.meshGenInputFolder;
+            Console.WriteLine("Process meshes relative to {0}", meshGenLocation);
+
+            // inventory the meshes to be transformed
+            CollateWeapons();
+            CollateShields();
+            CollateStatics();
+
+            // transform the meshes
+            TransformMeshes();
+        }
+    }
+}
---
+++ AllGUDMeshGen/SkeletonHandler.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using nifly;
+
+namespace AllGUD
+{
+    internal class SkeletonHandler
+    {
+        private static string? skeletonMeshLocation;
+        private static readonly string skeletonMeshFolder = "meshes/actors/character/";
+        private static readonly string skeletonMeshFilter = "*skeleton*.nif";
+
+        // patchable Nodes
+        private static ISet<string> weaponNodes = new HashSet<string>
+        {
+            "WeaponSword",
+            "WeaponDagger",
+            "WeaponAxe",
+            "WeaponMace",
+            "WeaponBack",
+            "WeaponBow"
+        };
+        // patched skeleton HeaderStrings
+        private static ISet<string> skeletonPatches = new HashSet<string>
+        {
+            "WeaponSwordArmor",
+            "WeaponDaggerArmor",
+            "WeaponAxeArmor",
+            "WeaponMaceArmor",
+            "WeaponBackArmor",
+            "WeaponBowArmor"
+        };
+        // skeleton HeaderStrings used in validity checking
+        private static ISet<string> skeletonValid = new HashSet<string>
+        {
+            "WeaponSwordLeft",
+            "WeaponDaggerLeft",
+            "WeaponAxeLeft",
+            "WeaponMaceLeft",
+            "WeaponStaff",
+            "WeaponStaffLeft",
+            "ShieldBack"
+        };
+
+        private static void PatchWeaponNodes(NifFile nif, NiNode node, ISet<String> weapons)
+        {
+            var childNodes = node.GetChildren().GetRefs();
+            IDictionary<int, NiAVObject> patchTargets = new Dictionary<int, NiAVObject>();
+            foreach (var childNode in childNodes)
+            {
+                var nodeBlock = nif.GetHeader().NiAVObjectBlock(childNode.GetIndex());
+                if (weapons.Contains(nodeBlock.GetName()))
+                {
+                    // Mark for patching and remove from target list - if we patch here, the loop
+                    // range gets nuked
+                    weapons.Remove(nodeBlock.GetName());
+                    patchTargets[childNode.GetIndex()] = nodeBlock;
+
+                }
+                else
+                {
+                    PatchWeaponNodes(nif, nif.GetHeader().NiNodeBlock(childNode.GetIndex()), weapons);
+                }
+                foreach (var patchTarget in patchTargets)
+                {
+                    NiAVObject newBlock = patchTarget.Value.Clone();
+                    newBlock.SetName(patchTarget.Value.GetName() + "Armor");
+
+                    // record new block and add as a sibling of existing
+                    int newID = nif.GetHeader().AddBlock(newBlock);
+                    node.GetChildren().AddBlockRef(newID);
+
+                    Console.WriteLine("Patched Weapon at Node {0}/{1} as new Block {2}/{3}",
+                        patchTarget.Key, patchTarget.Value.GetName(), newID, newBlock.GetName());
+                }
+                if (weapons.Count == 0)
+                    break;
+            }
+        }
+        private static void PatchSkeleton(string nifName)
+        {
+            Console.WriteLine("Skeleton Mesh {0}", nifName);
+            using (NifFile nif = new NifFile())
+            {
+                nif.Load(nifName);
+                ISet<string> headerStrings = new HashSet<string>();
+                for (int strId = 0; strId < nif.GetHeader().GetStringCount(); ++strId)
+                {
+                    headerStrings.Add(nif.GetHeader().GetStringById(strId));
+                }
+                ISet<string> patchedNodes = new HashSet<string>(skeletonPatches);
+                patchedNodes.IntersectWith(headerStrings);
+                if (skeletonPatches.Count == patchedNodes.Count)
+                {
+                    Console.WriteLine("This Skeleton already has the required AllGUD Armor Nodes", nifName);
+                    return;
+                }
+                if (!skeletonValid.IsSubsetOf(headerStrings))
+                {
+                    Console.WriteLine("This skeleton is missing one or more required Nodes ", nifName);
+                }
+                // Add missing required strings from patch node list
+                ISet<string> nodesToAdd = new HashSet<string>(skeletonPatches);
+                nodesToAdd.ExceptWith(patchedNodes);
+                foreach (string patchNode in nodesToAdd)
+                {
+                    Console.WriteLine("This Skeleton needs required AllGUD Armor Node {0}", patchNode);
+                    nif.GetHeader().AddOrFindStringId(patchNode);
+                }
+                // iterate blocks in the NIF
+                bool confirmedHuman = false;
+                for (int blockID = 0; blockID < nif.GetHeader().GetNumBlocks(); ++blockID)
+                {
+                    string blockType = nif.GetHeader().GetBlockTypeStringById(blockID);
+                    if (blockType == "NiNode")
+                    {
+                        if (!confirmedHuman)
+                        {
+                            NiNode node = nif.GetHeader().NiNodeBlock(blockID);
+                            // scan block refs checking for Extra Data with species=human
+                            var children = nif.StringExtraDataChildren(node, true);
+                            foreach (NiStringExtraData extraData in children)
+                            {
+                                var refs = extraData.GetStringRefList();
+                                if (refs.Count != 2)
+                                    continue;
+                                if (refs[0].GetString() == "species" && refs[1].GetString() == "Human")
+                                {
+                                    Console.WriteLine("This Skeleton is confirmed to be Human");
+                                    confirmedHuman = true;
+                                    break;
+                                }
+                            }
+                            if (!confirmedHuman)
+                                break;
+
+                            // find child weapon nodes in the graph
+                            ISet<string> targets = new HashSet<string>(weaponNodes);
+                            PatchWeaponNodes(nif, node, targets);
+                            if (targets.Count < weaponNodes.Count)
+                            {
+                                string newNif = Path.GetFileName(nifName);
+                                string relativePath = Path.GetRelativePath(skeletonMeshLocation!, Path.GetDirectoryName(nifName)!);
+                                string destFolder = ScriptLess.Configuration?.skeletonOutputFolder + relativePath;
+                                ScriptLess.CheckDestinationExists(destFolder);
+                                newNif = Path.Join(destFolder, newNif);
+                                Console.WriteLine("All Weapon nodes patched for Skeleton, saving to {0}", newNif);
+                                nif.Save(newNif, ScriptLess.saveOptions);
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        // Skeleton Patcher logic from AllGUD Skeleton Patcher.pas
+        public static void PatchAllHuman()
+        {
+            // determine the file path for meshes
+            skeletonMeshLocation = String.IsNullOrEmpty(ScriptLess.Configuration!.skeletonInputFolder) ?
+                (ScriptLess.PatcherState!.DataFolderPath + '/' + skeletonMeshFolder) : ScriptLess.Configuration.skeletonInputFolder;
+            Console.WriteLine("Process meshes relative to {0}", skeletonMeshLocation);
+
+            EnumerationOptions scanRule = new EnumerationOptions();
+            scanRule.RecurseSubdirectories = true;
+
+            foreach (string candidate in Directory.EnumerateFiles(skeletonMeshLocation, skeletonMeshFilter, scanRule))
+            {
+                PatchSkeleton(candidate);
+            }
+        }
+    }
+}
